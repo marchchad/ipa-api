adfcffd0b9c92693e342ebc6bc170f06eed33824
marchchad <marchchad@gmail.com>
2017-09-14 18:06:58 -0700

    Implementing webpack and created new gulp build to handle dev work.

---
+++ app/scripts/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by marchchad on 9/13/17.
+ */
+
+export default function start (){
+    alert("What about now??");
+}
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+/**
+ * Created by marchchad on 9/13/17.
+ */
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+    // ...
+    plugins: [
+        new BrowserSyncPlugin(
+            // BrowserSync options
+            {
+                // browse to http://localhost:3000/ during development
+                host: 'localhost',
+                port: 3000,
+                // proxy the Webpack Dev Server endpoint
+                // (which should be serving on http://localhost:3100/)
+                // through BrowserSync
+                proxy: 'http://localhost:3100/'
+            },
+            // plugin options
+            {
+                // prevent BrowserSync from reloading the page
+                // and let Webpack Dev Server take care of this
+                reload: false
+            }
+        )
+    ]
+}
